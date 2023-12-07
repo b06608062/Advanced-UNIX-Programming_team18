821e84b93db7cde23b272814a97024b782fc24ea
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-12-07 18:29:47 +0800

    ...

---
+++ midterm2/q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+  int count = 0;
+  time_t raw_time;
+  time(&raw_time);
+  printf("Loop %d current seconds: %d\n", count, localtime(&raw_time)->tm_sec);
+  while (1) {
+    for (int i = 0; i < 6; i++) {
+      sleep(10);
+    }
+
+    time(&raw_time);
+    printf("Loop %d current seconds: %d\n", ++count,
+           localtime(&raw_time)->tm_sec);
+  }
+
+  return 0;
+}
